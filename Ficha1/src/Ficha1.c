a98951af443c948ddbddce5a9856bb46e449424b
ArturCorreia02 <114989297+ArturCorreia02@users.noreply.github.com>
2022-10-14 19:54:40 +0100

    ficha2

---
+++ Ficha1/src/Ficha1.c
@@ -0,0 +1,82 @@
+/*
+ ============================================================================
+ Name        : Ficha1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+//======================= Ailea1===========================
+int main(void) {
+	float pi = 3.14;
+
+	float raio = 2.5;
+
+	float area = pi * raio * raio;
+//======================= Alinea 3=============================
+	int a = 5;
+	int b = 3;
+	int c = 7;
+	int d = 10;
+	int e = 1;
+
+	float numeros = 5;
+
+	float media = (a + b + c + d + e) / 5.0;
+
+	printf("A media dos 5 valores e: %f \n", numeros, media);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//===================== Alinea 5 =============================
+
+int horas = 8;
+int min = 22;
+int seg = 10;
+int total = horas * 3600 + min * 60 + seg;
+printf("total %d",total);
+
+return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ficha2/src/ficha2.c
@@ -0,0 +1,60 @@
+/*
+ ============================================================================
+ Name        : ficha2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(void) {
+//================================ Alinea 1 =================================
+	float p1 = 16.0;
+	float p2 = 13.0;
+	float freq = 15.0;
+	float avaliacao = (p1 * 0.3) + (p2 * 0.3) + (freq * 0.4);
+	printf("nota final é %f /n", avaliacao);
+	//================================ Alinea 2 =================================
+float base = 4.0;
+float altura = 11.0;
+
+float area = base * altura;
+
+printf("A área é %f \n", area);
+//================================ Alinea 3 =================================
+
+float comprimento =30.0;
+float largura =13.0;
+float altura =11.0;
+float volume = comprimento
+
+
+
+
+
+
+
+
+
+
+
+
+
+float ff = sqrt(4);
+float exp = pow(2, 3);
+
+int x1 = 1;
+int y1 = 2;
+
+int x2 = 3;
+int y2 = 3;
+
+float distance = sqrt(pow(x2 - x1, 2) +pow(y2 - y1, 2));
+
+printf("%f", exp);
+	return EXIT_SUCCESS;
+}
