91341652854924f93b08f687839968349c234434
ArturCorreia02 <114989297+ArturCorreia02@users.noreply.github.com>
2022-11-04 19:50:15 +0000

    ficha4

--- ficha4/src/ficha4.c
+++ ficha4/src/ficha4.c
@@ -10,12 +10,59 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+/*
+//=========================== alinea1 =======================================//
+float calculateCilinderVolume(float radius, float height){
+	float area = M_PI * pow(radius, 2);
+	float volume = area * height;
+	return volume;
+}
+*/
+//=========================== alinea2 =======================================//
+/*
+float calcularIMC(float peso, float altura){
+	float IMC = peso / pow(altura, 2);
+	return IMC;
+
+}
+
+void mensagemIMC(float condicao){
+	if(condicao < 18.5){
+		puts("Abaixo do peso.");
+	}else if (condicao >= 18.5 && condicao < 25){
+			puts("Peso normal.");
+		}else if (condicao >= 25 && condicao < 30){
+			puts("Assima do peso.");
+		}else{
+			puts("Obeso.");
+		}
+}
+*/
+//=========================== alinea3 =======================================//
+
+char desenhaRetangulos (char *){
+
+	return *;
+}
 
 int main(void) {
+/*
+	//=========================== alinea1=======================================//
+	//float radius = 2;
+	//float height = 4;
+	//float volumeC = calculateCilinderVolume(radius, height);
+	//printf("O cilindro de raio %f e altura %f tem o volume %f.", radius, height, volumeC);
 
-	for(int i = 0; i < 20; i+=5){
-	printf("%i \n", i);
-	}
+	//=========================== alinea2=======================================//
+
+	float peso = 70;
+	float altura = 1.73;
+	float condicao = calcularIMC(peso, altura);
+	//printf("O IMC de uma pessoa com %f de altura e %f peso é %f", altura, peso, condicao);
+
+	mensagemIMC(condicao);
+*/
 
 	return EXIT_SUCCESS;
 }
---
+++ teste/src/teste.c
@@ -0,0 +1,32 @@
+/*
+ ============================================================================
+ Name        : teste.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	/* local variable declaration
+	int a, b;
+	int c;
+
+	/* actual initialization
+	a = 10;
+	b = 20;
+	c = a + b;
+
+	printf("value of a = %d, b = %d and c = %d\n", a, b, c)*/
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
